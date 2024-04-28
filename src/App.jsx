3b76e438cdb7a136dee0b03affb8da0711f2a3b2
automationblog <sawin24@gmail.com>
2024-04-28 18:15:21 +0530

    price card task

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import PriceCard from './components/price_card/pricecard.jsx'
+
+function App() {
+
+  return (
+    <>
+        <PriceCard />
+    </>
+  )
+}
+
+export default App
